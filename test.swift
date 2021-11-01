6e768696ed367584dab9562df5946ccdfe0d710f
Eduardo Carmo <89488798+eduardocDev@users.noreply.github.com>
2021-11-01 22:49:58 +0000

    new updates!!

---
+++ test.swift
@@ -0,0 +1,13 @@
+var list = ["batata","abobora","cenoura","cebola","chuchu","abacaxi","limão","tomate","coco","maça"]
+
+list.sort(by: <)
+
+print(list)
+
+list.sort(by: >)
+
+print(list)
+
+print("this array have: \(list.count) elements")
+
+//testing things
