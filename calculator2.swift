b48f13de626c1f76d185a0fc5ef058a1bede7ee9
Eduardo Carmo <89488798+eduardocDev@users.noreply.github.com>
2021-11-07 01:25:43 +0000

    update!

--- calculator2.swift
+++ calculator2.swift
@@ -1,10 +1,15 @@
-//Inpue
+//Input
+print("""
+  ---------------------------------------------
+   Bem vindo a Hyper Mega Powerfull Calculadora
+  ---------------------------------------------
+""")
 print("Digite um valor")
 var value1 = readLine()
-print("Digite outro valor")
-var value2 = readLine()
 print("digite um operador: + - * / ")
 var operador = readLine()
+print("Digite outro valor")
+var value2 = readLine()
 
 //Process
 func sum(n1: Int, n2: Int){
@@ -24,15 +29,18 @@ func divide(n1: Int, n2: Int){
 var convertValue1 = Int(value1!)
 var convertValue2 = Int(value2!)
 //Output
-
 if operador! == "+"{
+  print("O total da soma destes valores é: ")
   print(sum(n1: convertValue1!, n2: convertValue2!))
 }else if  operador! == "-"{
+  print("O total da subtração destes valores é:")
   print(subtract(n1: convertValue1!, n2: convertValue2!))
 }else if operador! == "*"{
-  print(subtract(n1: convertValue1!, n2: convertValue2!))
+  print("O total da multiplicação destes valores é:")
+  print(multiply(n1: convertValue1!, n2: convertValue2!))
 }else if operador! == "/"{
-  print(subtract(n1: convertValue1!, n2: convertValue2!))
+  print("O total da divisão destes valores é:")
+  print(divide(n1: convertValue1!, n2: convertValue2!))
 }else{
   print("ERROR 404")
 }
